278943b714d2a3cc405c083395231571080c8912
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-07 12:18:50 -0400

    added modal from bootstrap // HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+// Retrieve buttons from HTML
+const addBtn = $('#add-btn');
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -36,3 +39,21 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'New message to ' + recipient
+  modalBodyInput.value = recipient
+})
