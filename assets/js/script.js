2d48df329810fbe7f19403a609e7b8f7aafd6694
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-07 23:20:14 -0400

    added datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // //Retrieve tasks inputs from add task form
 // const titleInput = $('#title').val()
-// const dateInput = $('#date').val()
+// const dateInput = $('#datepicker').val()
 // const descriptionInput = $('#description').val()
 
 
@@ -23,33 +23,33 @@ function generateTaskId(prefix) {
 function createTaskCard(task) {
     // create element for the card using jquery + bootstrap
     const taskCard = $('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p><button type="button" class="btn btn-success mx-auto" id="delete-btn">Delete</button></div></div>');
-    
+
     // call generateTaskId function and assign to task card
     const taskId = generateTaskId('task');
     // set the attribute of the task card with unique id
     taskCard.attr('id', taskId)
-    
+
     // retrieve parent element
     const todoDiv = $('#todo-cards');
     //append taskCard to parent
     todoDiv.append(taskCard);
-    
+
     console.log(taskId);
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    
-    
+
+
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -60,6 +60,14 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+
+    //adds datepicker
+    $(function () {
+        $("#datepicker").datepicker();
+    });
+
+    //makes card <div>s droppable
+
 
 });
 
