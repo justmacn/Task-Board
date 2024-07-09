9928a79c9d217e9d4a7b854f602f50293539aaa1
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-09 10:14:08 -0400

    debugged createTaskCard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,64 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// //Retrieve tasks inputs from add task form
-// const titleInput = $('#title').val()
-// const dateInput = $('#datepicker').val()
-// const descriptionInput = $('#description').val()
+//Retrieve tasks inputs from add task form
+const titleInput = $('#title');
+const dateInput = $('#date');
+const descriptionInput = $('#description');
 
-// sets the starting uniqueId number
-let taskIdCounter = 1
 
 // A function that generates a unique task id
-function generateTaskId(prefix) {
-    
-    // concatenates the id number and our prefix parameter ('task') 
-    const uniqueId = prefix + '-' + taskIdCounter;
-    // increments the id number with each task created
-    taskIdCounter++;
-    return uniqueId;
+function generateTaskId() {
+    let taskId = crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    // create element for the card using jquery + bootstrap
-    const taskCard = $('<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p><button type="button" class="btn btn-success mx-auto" id="delete-btn">Delete</button></div></div>');
+    // create elements for the card using bootstrap classes
+    // 1. create element, 2. add attributes to element, 3. append element to page
+    const taskCard = $('<div>').addClass('card task-card draggable my-3').css('width', '18rem').attr('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h5').text(task.title);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text text-center').text(task.description);
+    const cardDate = $('<p>').addClass('card-text text-center').text(task.dueDate);
+    const cardBtn = $('<button>').addClass('btn btn-danger delete mx-auto').attr('data-task-id', task.id).text('Delete');
+    
+    // this event calls the deleteTask function on click
+    cardBtn.on('click', handleDeleteTask);
+    
+    // conditionals that changes card color based on dueDate
+    if (task.dueDate && task.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+        
+        if (now.isSame(taskDueDate, 'day')) {
+            taskCard.addClass('bg-warning text-white');
+          } else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass('bg-danger text-white');
+            cardBtn.addClass('border-light');
+          }
+    }
 
-    // call generateTaskId function and assign to task card
-    const taskId = generateTaskId('task');
-    // set the attribute of the task card with unique id
-    taskCard.attr('id', taskId)
+    // append all elements of taskCard to the div
+    cardBody.append(cardDescription, cardDate, cardBtn);
+    taskCard.append(cardHeader, cardBody);
 
-};
+    return taskCard;
+}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
-    createTaskCard();
-        
-    // retrieve parent element
-    const todoDiv = $('#todo-cards');
-    //append taskCard to parent
-    todoDiv.append(taskCard);
     
-    console.log(taskId);
-
+    
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {
+function handleAddTask(event){
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {
+function handleDeleteTask(event){
 
 }
 
@@ -64,20 +71,8 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
-    // adds datepicker
-    $(function () {
-        $("#datepicker").datepicker();
-    });
-
-    // makes card <div> droppable
-
-    // eventListener to call createTaskCard function
-    const submitBtn = $('#submit-btn');
-
-    submitBtn.on('click', function () {
-
-        renderTaskList();
-    });
-
 });
 
+const submitBtn = $('#submit-btn');
+
+submitBtn.on('click', createTaskCard())
