7d92f72ff6105862b3d627c23a0f45446dd7c9d1
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-09 21:23:22 -0400

    added handleAddTask function // JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,21 +29,21 @@ function createTaskCard(task) {
     const cardDescription = $('<p>').addClass('card-text text-center').text(task.description);
     const cardDate = $('<p>').addClass('card-text text-center').text(task.dueDate);
     const cardBtn = $('<button>').addClass('btn btn-danger delete mx-auto').attr('data-task-id', task.id).text('Delete');
-    
+
     // this event calls the deleteTask function on click
     cardBtn.on('click', handleDeleteTask);
-    
+
     // conditionals that changes card color based on dueDate
     if (task.dueDate && task.status !== 'done') {
         const now = dayjs();
         const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
-        
+
         if (now.isSame(taskDueDate, 'day')) {
             taskCard.addClass('bg-warning text-white');
-          } else if (now.isAfter(taskDueDate)) {
+        } else if (now.isAfter(taskDueDate)) {
             taskCard.addClass('bg-danger text-white');
             cardBtn.addClass('border-light');
-          }
+        }
     }
 
     // append all elements of taskCard to the div
@@ -68,40 +68,72 @@ function renderTaskList() {
     // create a taskCard for each task in the taskList array
     for (let task of tasks) {
         if (tasks.status === 'to-do') {
-          todoLane.append(createTaskCard(task));
+            todoLane.append(createTaskCard(task));
         } else if (project.status === 'in-progress') {
-          inProgressLane.append(createProjectCard(task));
+            inProgressLane.append(createProjectCard(task));
         } else if (project.status === 'done') {
-          doneLane.append(createProjectCard(task));
+            doneLane.append(createProjectCard(task));
         }
-      }
-      
-      // makes elements draggable that hold the 
-      $('.draggable').draggable({
+    }
+
+    // makes elements draggable that hold the 
+    $('.draggable').draggable({
         opacity: 0.7,
         zIndex: 100,
-      })
+    })
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    
+function handleAddTask(event) {
+    event.preventDefault();
+
+    // call the id generator function
+    generateTaskId(taskId);
+
+
+    // declare form inputs as variables to gain their values
+    const taskTitle = titleInput.val().trim();
+    const taskDate = dateInput.val();
+    const taskDescription = descriptionInput.val().trim(); // yyyy-mm-dd format
+
+    // create an object out of the addTask form values
+    const newTask = {
+        id: taskId.val(),
+        title: taskTitle,
+        dueDate: taskDate,
+        description: taskDescription,
+        status: 'to-do',
+    };
+
+    // pushes new Task to the array
+    const tasks = taskList;
+    tasks.push(newTask);
+
+    // save newTask to taskList array and render them to page
+    saveTaskstoStorage(tasks);
+    renderTaskList();
+
+    // clear form inputs
+    titleInput.val('');
+    dateInput.val('');
+    descriptionInput.val('');
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {
-  const taskId = $(this).attr('data-project-id');
-  const tasks = taskList;
+    const taskId = $(this).attr('data-project-id');
+    const tasks = taskList;
 
-  // this conditional goes through the taskList array and removes the task with the matching id
-  tasks.forEach((task) => {
-    if (task.id === taskId) {
-      tasks.splice(tasks.indexOf(task), 1);
-    }
-  });
+    // this conditional goes through the taskList array and removes the task with the matching id
+    tasks.forEach((task) => {
+        if (task.id === taskId) {
+            tasks.splice(tasks.indexOf(task), 1);
+        }
+    });
 
-  saveTaskstoStorage(tasks);
-  renderTaskList();
+    // saves the updated taskList and renders them to page
+    saveTaskstoStorage(tasks);
+    renderTaskList();
 
 }
 
@@ -118,9 +150,14 @@ $(document).ready(function () {
     $('#date').datepicker({
         changeMonth: true,
         changeYear: true,
+    });
+
+    $('.lane').droppable({
+        accept: '.draggable',
+        drop: handleDrop,
       });
 
-      const submitBtn = $('#submit-btn');
-      
-      submitBtn.on('click', createTaskCard())
+    const submitBtn = $('#submit-btn');
+
+    submitBtn.on('click', handleAddTask())
 });
